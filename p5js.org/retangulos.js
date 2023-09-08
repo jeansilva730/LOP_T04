5c06839c3430fe307d82d5056f8142eca1efe766
jeansilva730 <144482673+jeansilva730@users.noreply.github.com>
2023-09-08 15:39:50 -0300

    Create retangulos.js

---
+++ p5js.org/retangulos.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill('blue');
+  rect(200, 200, 30, 30);
+  fill('Yellow');
+  rect(200, 250, 100, 30);
+}
