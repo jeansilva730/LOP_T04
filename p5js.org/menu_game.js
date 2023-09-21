4b887f35f313525e43eb84898b488c9bee029477
jeansilva730 <144482673+jeansilva730@users.noreply.github.com>
2023-09-21 15:04:47 -0300

    Update menu_game.js

--- p5js.org/menu_game.js
+++ p5js.org/menu_game.js
@@ -2,6 +2,15 @@ const CANVAS_W = 600;
 const CANVAS_H = 400;
 const SCREEN_CENTER_W = CANVAS_W / 2; // Largura/Centro tela
 const SCREEN_CENTER_H = CANVAS_H / 2; // Altura/Centro tela
+var screen_is = "MENU";
+var btn_start_color = "grey";
+var btn_start_text = "white";
+var btn_options_color = "grey";
+var btn_options_text = "white";
+var btn_credits_color = "grey";
+var btn_credits_text = "white";
+var value = 0;
+
 
 /*
 function actor(){
@@ -13,29 +22,27 @@ function menu() {
   const MENU_START = "Iniciar";
   const MENU_OPTIONS = "Opções";
   const MENU_CREDITS = "Créditos";
-  const BTN_W = CANVAS_W * 0.25; // Largura do botão
-  const BTN_H = CANVAS_H * 0.10; // Altura do botão
-  var BTN_START_COLOR = "grey";
-  var BTN_START_TEXT = "white";
+  var BTN_W = CANVAS_W * 0.25; // Largura do botão
+  var BTN_H = CANVAS_H * 0.10; // Altura do botão
   rectMode(CENTER);
   // Start button
-  fill('grey');
+  fill(btn_start_color);
   rect(SCREEN_CENTER_W, SCREEN_CENTER_H, BTN_W, BTN_H);
-  fill('white');
+  fill(btn_start_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
   text(MENU_START, SCREEN_CENTER_W, SCREEN_CENTER_H + 8, BTN_W, BTN_H);
   // Options button
-  fill('grey');
+  fill(btn_options_color);
   rect(SCREEN_CENTER_W, SCREEN_CENTER_H + 50, BTN_W, BTN_H);
-  fill('white');
+  fill(btn_options_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
   text(MENU_OPTIONS, SCREEN_CENTER_W, SCREEN_CENTER_H + 58, BTN_W, BTN_H);
   // Créditos button
-  fill('grey');
+  fill(btn_credits_color);
   rect(SCREEN_CENTER_W, SCREEN_CENTER_H + 100, BTN_W, BTN_H);
-  fill('white');
+  fill(btn_credits_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
   text(MENU_CREDITS, SCREEN_CENTER_W, SCREEN_CENTER_H + 108, BTN_W, BTN_H);
@@ -43,11 +50,31 @@ function menu() {
   return 0;
 }
 
+function debugMouse(){
+  fill('black');
+  textSize(16);
+  text("X: " + mouseX + "\nY: " + mouseY, 40, 20);
+  // Cursor
+  fill('white');
+  if (value === 1){ fill('orange');}
+  ellipse(mouseX, mouseY, 10, 10);
+};
 function setup() {
   createCanvas(CANVAS_W, CANVAS_H);
+  noCursor();
 }
 
 function draw() {
   background(220);
   menu();
+  debugMouse();
+}
+
+function mouseMoved(){
+  // debug mouse
+  if (mouseX > 100){
+    value = 1;
+  }
+  // prevent default
+  return false;
 }
