21546a40769ce12d9a89ff50f3a8c928de142efa
jeansilva730 <144482673+jeansilva730@users.noreply.github.com>
2023-10-11 16:01:17 -0300

    Update menu_game.js

--- p5js.org/menu_game.js
+++ p5js.org/menu_game.js
@@ -1,16 +1,20 @@
 // Tamanho da Tela (w = Largura, H = Altura)
-const CANVAS_W = 600;
-const CANVAS_H = 400;
+const CANVAS_X = 600;
+const CANVAS_Y = 400;
 // Centro da tela
-const SCREEN_CENTER_W = CANVAS_W / 2; // Largura/Centro tela
-const SCREEN_CENTER_H = CANVAS_H / 2; // Altura/Centro tela
+const SCREEN_CENTER_X = CANVAS_X / 2; // Largura/Centro tela
+const SCREEN_CENTER_Y = CANVAS_Y / 2; // Altura/Centro tela
+const MENU_START = "Iniciar";
+const MENU_CONTROLLERS = "Controles";
+const MENU_CREDITS = "Créditos";
+// Variável de controle de tela
 var screen_is = "MENU";
 // Cores iniciais dos botões
 var btn_start_color = "green";
 var btn_start_text = "white";
-var btn_options_color = "grey";
-var btn_options_text = "white";
-var btn_credits_color = "grey";
+var btn_controllers_color = "blue";
+var btn_controllers_text = "white";
+var btn_credits_color = "blue";
 var btn_credits_text = "white";
 // Variável de controle para o mouse
 var btn_mouse_moved = 0;
@@ -21,6 +25,7 @@ function actor(){
   ellipseMode()
 }
 */
+
 function changeScreen() {
   if (screen_is == 'MENU'){
     menu();
@@ -30,52 +35,94 @@ function changeScreen() {
 }
 
 function menu() {
-  const MENU_START = "Iniciar";
-  const MENU_OPTIONS = "Opções";
-  const MENU_CREDITS = "Créditos";
   // Definição do tamanho dos botões proporcional
   // ao tamanho da tela
-  var BTN_W = CANVAS_W * 0.25; // Largura do botão
-  var BTN_H = CANVAS_H * 0.10; // Altura do botão
+  var BTN_W = CANVAS_X * 0.25; // Largura do botão
+  var BTN_H = CANVAS_Y * 0.10; // Altura do botão
   // Start button
   fill(btn_start_color);
   if (btn_mouse_moved === 1){
     fill('red');
   }
-  rect(SCREEN_CENTER_W, SCREEN_CENTER_H, BTN_W, BTN_H);
+  rect(SCREEN_CENTER_X, SCREEN_CENTER_Y, BTN_W, BTN_H);
   fill(btn_start_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
-  text(MENU_START, SCREEN_CENTER_W, SCREEN_CENTER_H + 8, BTN_W, BTN_H);
-  // Options button
-  fill(btn_options_color);
+  text(MENU_START, SCREEN_CENTER_X, SCREEN_CENTER_Y + 8, BTN_W, BTN_H);
+  // Controllers button
+  fill(btn_controllers_color);
   if (btn_mouse_moved === 2){
     fill('red');
   }
-  rect(SCREEN_CENTER_W, SCREEN_CENTER_H + 50, BTN_W, BTN_H);
-  fill(btn_options_text);
+  rect(SCREEN_CENTER_X, SCREEN_CENTER_Y + 50, BTN_W, BTN_H);
+  fill(btn_controllers_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
-  text(MENU_OPTIONS, SCREEN_CENTER_W, SCREEN_CENTER_H + 58, BTN_W, BTN_H);
+  text(MENU_CONTROLLERS, SCREEN_CENTER_X, SCREEN_CENTER_Y + 58, BTN_W, BTN_H);
   // Créditos button
   fill(btn_credits_color);
   if (btn_mouse_moved === 3){
     fill('red');
   }
-  rect(SCREEN_CENTER_W, SCREEN_CENTER_H + 100, BTN_W, BTN_H);
+  rect(SCREEN_CENTER_X, SCREEN_CENTER_Y + 100, BTN_W, BTN_H);
   fill(btn_credits_text);
   textAlign(CENTER);
   textSize((BTN_W + BTN_H) / 6);
-  text(MENU_CREDITS, SCREEN_CENTER_W, SCREEN_CENTER_H + 108, BTN_W, BTN_H);
+  text(MENU_CREDITS, SCREEN_CENTER_X, SCREEN_CENTER_Y + 108, BTN_W, BTN_H);
   return 0;
 }
 
 function levels(){
+  let lvl = 1;
   for (let y = 80; y <= 320; y += 80){
     for (let x = 80; x <= 320; x += 80){
       fill('orange');
       rect(x, y, 50);
+      fill('black');
+      textSize(16);
+      text("Lv " + lvl, x, y);
+      lvl++;
     }
+  }
+  fill("#8B8BEC");
+  rect(500, 200, 75, 25);
+  fill("black");
+  text("Retornar", 500, 200);
+}
+
+function returnScreen(){
+  if ( (mouseX > (500-75/2) && mouseX < (500+75/2)) && (mouseY > 175 && mouseY < (200+25/2)) ){
+    screen_is = "MENU";
+  }
+}
+
+function mouseMenu(){
+  // Verifica se o mouse está em cima do botão 'Iniciar' 
+  if ((mouseX > 220 && mouseX < 380) && (mouseY > 175 && mouseY < 225)) {
+    screen_is = "INICIAR";
+  }// Verifica se o mouse está em cima do botão 'Opções'
+   else if ((mouseX > 220 && mouseX < 380) && (mouseY > 230 && mouseY < 272)) {
+    screen_is = "CONTROLES";
+  } // Verifica se o mouse está em cima do botão 'Créditos'
+  else if ((mouseX > 220 && mouseX < 380) && (mouseY > 276 && mouseY < 323)) {
+    screen_is = "CREDITOS";
+  } else {
+    screen_is = "MENU";
+  }
+}
+
+function menuButtons(){
+    // Verifica se o mouse está em cima do botão 'Iniciar' 
+  if ((mouseX > 220 && mouseX < 380) && (mouseY > 175 && mouseY < 225)) {
+    btn_mouse_moved = 1;
+  }// Verifica se o mouse está em cima do botão 'Opções'
+   else if ((mouseX > 220 && mouseX < 380) && (mouseY > 230 && mouseY < 272)) {
+    btn_mouse_moved = 2;
+  } // Verifica se o mouse está em cima do botão 'Créditos'
+  else if ((mouseX > 220 && mouseX < 380) && (mouseY > 276 && mouseY < 323)) {
+    btn_mouse_moved = 3;
+  } else {
+    btn_mouse_moved = 0;
   }
 }
 
@@ -90,41 +137,23 @@ function debugMouse(){
 
 function mouseMoved(){
   // Se o mouse se mover, ele irá checar todas essas ações abaixo.
-  // Verifica se o mouse está em cima do botão 'Iniciar' 
-  if ((mouseX > 220 && mouseX < 380) && (mouseY > 175 && mouseY < 225)) {
-    btn_mouse_moved = 1;
-  }// Verifica se o mouse está em cima do botão 'Opções'
-   else if ((mouseX > 220 && mouseX < 380) && (mouseY > 230 && mouseY < 272)) {
-    btn_mouse_moved = 2;
-  } // Verifica se o mouse está em cima do botão 'Créditos'
-  else if ((mouseX > 220 && mouseX < 380) && (mouseY > 276 && mouseY < 323)) {
-    btn_mouse_moved = 3;
-  } else {
-    btn_mouse_moved = 0;
-  }
+  menuButtons();
   // prevent default
   return false;
 }
 
 function mouseClicked(){
   // Verifica se o mouse está em cima do botão 'Iniciar' 
-  if ((mouseX > 220 && mouseX < 380) && (mouseY > 175 && mouseY < 225)) {
-    screen_is = "INICIAR";
-  }// Verifica se o mouse está em cima do botão 'Opções'
-   else if ((mouseX > 220 && mouseX < 380) && (mouseY > 230 && mouseY < 272)) {
-    screen_is = "OPCAO";
-  } // Verifica se o mouse está em cima do botão 'Créditos'
-  else if ((mouseX > 220 && mouseX < 380) && (mouseY > 276 && mouseY < 323)) {
-    screen_is = "CREDITOS";
-  } else {
-    screen_is = "MENU";
+  if (screen_is === "MENU"){
+    mouseMenu();
   }
+  returnScreen();
   // prevent default
   return false;
 }
 
 function setup() {
-  createCanvas(CANVAS_W, CANVAS_H);
+  createCanvas(CANVAS_X, CANVAS_Y);
   rectMode(CENTER);
   noCursor();
 }
